4b749d3bf5c7425755c3b02164be678f1d506176
TryingToImprove <privat.oliver.lassen@gmail.com>
2017-02-07 20:05:53 +0100

    Make it possible to input triangles

--- src/Program.cs
+++ src/Program.cs
@@ -13,12 +13,47 @@ namespace TrinagleApplication
 
             var equilateralTriangle = new Triangle(3, 3, 3);
             Console.WriteLine($"equilateralTriangle: {triangleDeterminer.Determine(equilateralTriangle)}");
-            
+
             var invalidTriangle = new Triangle(5, 1, 1);
             Console.WriteLine($"invalidTriangle: {triangleDeterminer.Determine(invalidTriangle)}");
 
             var isoscelesTriangle = new Triangle(3, 4, 3);
             Console.WriteLine($"isoscelesTriangle: {triangleDeterminer.Determine(isoscelesTriangle)}");
+
+            // Lets make a custom
+            while (true)
+            {
+                var sideNames = new char[] { 'A', 'B', 'C' };
+                var sides = new int[3];
+
+                for (var i = 0; i < sides.Length; i++)
+                {
+                    while (true)
+                    {
+                        Console.Write($"Enter side {sideNames[i]}: ");
+
+                        // Get the value
+                        var input = Console.ReadLine();
+
+                        int sideValue;
+
+                        // Is the input a valid int?
+                        if (int.TryParse(input, out sideValue))
+                        {
+                            sides[i] = sideValue;
+                            break;
+                        }
+                        else
+                        {
+                            Console.Write("INVALID VALUE\n");
+                        }
+                    }
+                }
+
+                // Construct triangle
+                var customTriangle = new Triangle(sides[0], sides[1], sides[2]);
+                Console.WriteLine($"The triangle is {triangleDeterminer.Determine(customTriangle)}");
+            }
         }
     }
 }
